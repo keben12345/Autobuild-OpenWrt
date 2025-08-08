a5333747d5ccc6dd65f43401248130e815be93b6
xiaouex <36121363+xiaouex@users.noreply.github.com>
2025-08-08 21:35:35 +0800

    Update diy-part2.sh

--- config/diy-part2.sh
+++ config/diy-part2.sh
@@ -5,6 +5,9 @@ git clone --depth 1 https://github.com/gSpotx2f/luci-app-temp-status feeds/luci/
 git clone --depth 1 https://github.com/DustReliant/luci-app-filetransfer package/xiaouex/luci-app-filetransfer
 git clone --depth 1 https://github.com/pymumu/luci-app-smartdns feeds/packages/smpackage/luci-app-smartdns
 git clone --depth 1 https://github.com/pymumu/openwrt-smartdns feeds/packages/smpackage/smartdns
+sed -i '/PKG_MIRROR_HASH/d' feeds/packages/smpackage/smartdns/Makefile
+sed -i '/PKG_SOURCE_VERSION/d' feeds/packages/smpackage/smartdns/Makefile
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a 
 sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
