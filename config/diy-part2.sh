a0fd5cbf034721d7189d6a3ffb891c635d44ec98
xiaouex <36121363+xiaouex@users.noreply.github.com>
2025-08-08 16:01:53 +0800

    Update diy-part2.sh

--- config/diy-part2.sh
+++ config/diy-part2.sh
@@ -2,7 +2,7 @@
 rm -rf feeds/packages/smpackage/luci-app-smartdns
 git clone --depth 1 https://github.com/gSpotx2f/luci-app-temp-status feeds/luci/applications/luci-app-temp-status
 git clone --depth 1 https://github.com/DustReliant/luci-app-filetransfer package/xiaouex/luci-app-filetransfer
-git clone --depth 1 https://github.com/pymumu/smartdns feeds/packages/smpackage/luci-app-smartdns
+git clone --depth 1 https://github.com/pymumu/luci-app-smartdns feeds/packages/smpackage/luci-app-smartdns
 ./scripts/feeds update -a
 ./scripts/feeds install -a 
 sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
