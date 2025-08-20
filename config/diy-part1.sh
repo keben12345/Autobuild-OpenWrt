1c0d4c4d6533267cdf3990be003f7e8d25338962
xiaouex <36121363+xiaouex@users.noreply.github.com>
2025-08-20 13:26:20 +0800

    Update BORE scheduler patch to version 6.3.2
    Renamed and updated the BORE scheduler patch from 6.1.1 to 6.3.2, with significant changes to kernel scheduling files. Added futex boost support, new sysctl parameters, and improved SMT idle core preference. Updated diy-part1.sh to use the new patch and configuration options.

--- config/diy-part1.sh
+++ config/diy-part1.sh
@@ -19,7 +19,9 @@ cp $GITHUB_WORKSPACE/config/files/bbr3/601-*.patch target/linux/generic/hack-6.1
 cp $GITHUB_WORKSPACE/config/files/bbr3/501-*.patch package/network/utils/iproute2/patches
 cp $GITHUB_WORKSPACE/config/files/bbr3/502-*.patch package/network/utils/iproute2/patches
 cp $GITHUB_WORKSPACE/config/files/bbr3/500-*.patch package/network/utils/iproute2/patches
-cp $GITHUB_WORKSPACE/config/files/BORE-TEST/6.12/0001-linux6.12.37-bore-6.1.1.patch target/linux/generic/hack-6.12
+cp $GITHUB_WORKSPACE/config/files/BORE-TEST/6.12/0001-linux6.12.37-bore-6.3.2.patch target/linux/generic/hack-6.12
 cp $GITHUB_WORKSPACE/config/files/BORE-TEST/6.12/0002-sched-ext-coexistence-fix.patch target/linux/generic/pending-6.12
+cp $GITHUB_WORKSPACE/config/files/BORE-TEST/6.12/0002-sched-fair-Prefer-full-idle-SMT-cores.patch target/linux/generic/hack-6.12
 echo 'CONFIG_SCHED_BORE=y' >> target/linux/x86/config-6.12
+echo 'CONFIG_MIN_BASE_SLICE_NS=2000000' >> target/linux/x86/config-6.12
 sed -i '/label = "bl2";/,/};/ { /read-only;/d }' target/linux/mediatek/dts/mt7981b-cmcc-rax3000m-nand.dtso
