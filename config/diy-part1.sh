b59f3de61abfe5bfcde813546aa03257b7e482e7
xiaouex <36121363+xiaouex@users.noreply.github.com>
2026-02-07 19:19:45 +0800

    Update diy-part1.sh

--- config/diy-part1.sh
+++ config/diy-part1.sh
@@ -8,7 +8,7 @@ DEVICE="$MY_DEVICE"
 sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
+bash $GITHUB_WORKSPACE/config/add_turboacc.sh --no-sfe
 
 chmod +x $GITHUB_WORKSPACE/config/files/my-script/files/my_script
 mkdir -p package/xiaouex
