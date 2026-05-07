137986eebbea9a41058fece731d3baf22eff8538
xiaouex <36121363+xiaouex@users.noreply.github.com>
2026-05-07 17:32:08 +0800

    Update diy-part1.sh

--- config/diy-part1.sh
+++ config/diy-part1.sh
@@ -5,8 +5,8 @@ DEVICE="$MY_DEVICE"
 
 
 # Add feed sources
-#sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-sed -i '1i src-git jell https://github.com/kenzok8/jell' feeds.conf.default
+sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+#sed -i '1i src-git jell https://github.com/kenzok8/jell' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
 
