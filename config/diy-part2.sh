c479093d30cf5cd08b0aee523d1fef7a22a31770
xiaouex <36121363+xiaouex@users.noreply.github.com>
2026-05-07 14:52:26 +0800

    Update diy-part2.sh

--- config/diy-part2.sh
+++ config/diy-part2.sh
@@ -2,8 +2,8 @@
 
 git clone --depth 1 https://github.com/gSpotx2f/luci-app-temp-status feeds/luci/applications/luci-app-temp-status
 git clone --depth 1 https://github.com/DustReliant/luci-app-filetransfer package/xiaouex/luci-app-filetransfer
-#rm -rf feeds/smpackage/v2ray-geodata
-#git clone --depth 1 https://github.com/sbwml/v2ray-geodata feeds/smpackage/v2ray-geodata
+rm -rf feeds/smpackage/v2ray-geodata
+git clone --depth 1 https://github.com/sbwml/v2ray-geodata feeds/smpackage/v2ray-geodata
 ./scripts/feeds update -a
 ./scripts/feeds install -a 
 sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
