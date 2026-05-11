28b79ae9042c36122309e71bdf44dfa9489a9740
xiaouex <36121363+xiaouex@users.noreply.github.com>
2026-05-11 17:42:24 +0800

    Update diy-part1.sh

--- config/diy-part1.sh
+++ config/diy-part1.sh
@@ -10,10 +10,10 @@ sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.con
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 #curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
 
-chmod +x $GITHUB_WORKSPACE/config/files/my-script/files/my_script
-mkdir -p package/xiaouex
-mv -f $GITHUB_WORKSPACE/config/files/ipv6-helper package/xiaouex/ipv6-helper
-mv -f $GITHUB_WORKSPACE/config/files/my-script package/xiaouex/my-script
+#chmod +x $GITHUB_WORKSPACE/config/files/my-script/files/my_script
+#mkdir -p package/xiaouex
+#mv -f $GITHUB_WORKSPACE/config/files/ipv6-helper package/xiaouex/ipv6-helper
+#mv -f $GITHUB_WORKSPACE/config/files/my-script package/xiaouex/my-script
 
 #Add BBR V3
 
@@ -56,4 +56,4 @@ echo 'CONFIG_MIN_BASE_SLICE_NS=2000000' >> target/linux/mediatek/filogic/config-
 
 
 
-sed -i '/label = "bl2";/,/};/ { /read-only;/d }' target/linux/mediatek/dts/mt7981b-cmcc-rax3000m-nand.dtso
+#sed -i '/label = "bl2";/,/};/ { /read-only;/d }' target/linux/mediatek/dts/mt7981b-cmcc-rax3000m-nand.dtso
