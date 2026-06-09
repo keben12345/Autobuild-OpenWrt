14773bfad02801fe2b914e4e2419d85ef62d1d93
xiaouex <36121363+xiaouex@users.noreply.github.com>
2026-06-09 09:28:49 +0800

    Replace smartdns with mosdns in rax3000m config
    Comment out the removal/cloning of luci-app-smartdns in SETUP_rax3000m.sh (preserving the Makefile zlib dependency patch) and update config/rax3000m/.config to disable smartdns and its i18n while enabling mosdns and luci-app-mosdns. This swaps SmartDNS packages for mosdns in the rax3000m build configuration.

--- config/SETUP_rax3000m.sh
+++ config/SETUP_rax3000m.sh
@@ -56,8 +56,8 @@ function task_step_3() {
     cd $GITHUB_WORKSPACE/openwrt
     #git clone --depth 1 https://github.com/gSpotx2f/luci-app-temp-status feeds/luci/applications/luci-app-temp-status
 
-    rm -rf feeds/smpackage/luci-app-smartdns
-    git clone --depth 1 https://github.com/pymumu/luci-app-smartdns feeds/smpackage/luci-app-smartdns
+    #rm -rf feeds/smpackage/luci-app-smartdns
+    #git clone --depth 1 https://github.com/pymumu/luci-app-smartdns feeds/smpackage/luci-app-smartdns
     sed -i 's/DEPENDS:=+i386:libatomic +libopenssl/DEPENDS:=+i386:libatomic +libopenssl +zlib/g' feeds/smpackage/smartdns/Makefile
     #git clone --depth 1 https://github.com/DustReliant/luci-app-filetransfer package/xiaouex/luci-app-filetransfer
     #rm -rf feeds/smpackage/v2ray-geodata
