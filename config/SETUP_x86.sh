720e70fb5a3ab4378c1e605ec9b9558a0b28c7f9
xiaouex <36121363+xiaouex@users.noreply.github.com>
2026-06-09 17:52:15 +0800

    Update SETUP_x86.sh

--- config/SETUP_x86.sh
+++ config/SETUP_x86.sh
@@ -54,6 +54,7 @@ function task_step_3() {
 
     echo "正在执行 [步骤 3]: 添加额外软件包，并执行 feeds install和feeds install..."
     cd $GITHUB_WORKSPACE/openwrt
+    curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
     git clone --depth 1 https://github.com/gSpotx2f/luci-app-temp-status feeds/luci/applications/luci-app-temp-status
     #git clone --depth 1 https://github.com/DustReliant/luci-app-filetransfer package/xiaouex/luci-app-filetransfer
     #rm -rf feeds/smpackage/v2ray-geodata
