c454e85037e964fd76adf93ee1d3f8020a716b75
xiaouex <36121363+xiaouex@users.noreply.github.com>
2026-06-10 09:02:54 +0800

    Update SETUP_rax3000m.sh

--- config/SETUP_rax3000m.sh
+++ config/SETUP_rax3000m.sh
@@ -57,8 +57,8 @@ function task_step_3() {
     #git clone --depth 1 https://github.com/gSpotx2f/luci-app-temp-status feeds/luci/applications/luci-app-temp-status
 
     #rm -rf feeds/smpackage/luci-app-smartdns
-    #git clone --depth 1 https://github.com/pymumu/luci-app-smartdns feeds/smpackage/luci-app-smartdns
-    #sed -i 's/DEPENDS:=+i386:libatomic +libopenssl/DEPENDS:=+i386:libatomic +libopenssl +zlib/g' feeds/smpackage/smartdns/Makefile
+    git clone --depth 1 https://github.com/pymumu/luci-app-smartdns feeds/smpackage/luci-app-smartdns
+    sed -i 's/DEPENDS:=+i386:libatomic +libopenssl/DEPENDS:=+i386:libatomic +libopenssl +zlib/g' feeds/smpackage/smartdns/Makefile
     #git clone --depth 1 https://github.com/DustReliant/luci-app-filetransfer package/xiaouex/luci-app-filetransfer
     #rm -rf feeds/smpackage/v2ray-geodata
     #git clone --depth 1 https://github.com/sbwml/v2ray-geodata feeds/smpackage/v2ray-geodata
