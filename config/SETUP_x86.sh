4c68d49895747ff3b224ba0fa09437ebdb580672
xiaouex <36121363+xiaouex@users.noreply.github.com>
2026-07-21 19:12:31 +0800

    Update SETUP_x86.sh

--- config/SETUP_x86.sh
+++ config/SETUP_x86.sh
@@ -95,11 +95,11 @@ function task_step_5() {
     echo "正在执行 [步骤 5]: 合入自定义补丁..."
     #Add BORE Scheduler
     git clone -b main https://github.com/firelzrd/bore-scheduler $GITHUB_WORKSPACE/config/files/BORE
-    cp $GITHUB_WORKSPACE/config/files/BORE/patches/stable/linux-6.18-bore/*.patch target/linux/generic/hack-6.18
-    cp $GITHUB_WORKSPACE/config/files/BORE/patches/additions/*.patch target/linux/generic/pending-6.18
+    cp $GITHUB_WORKSPACE/config/files/BORE/patches/stable/linux-6.18-bore/*.patch openwrt/target/linux/generic/hack-6.18
+    cp $GITHUB_WORKSPACE/config/files/BORE/patches/additions/*.patch openwrt/target/linux/generic/pending-6.18
     rm -rf $GITHUB_WORKSPACE/config/files/BORE
     #Add bbr3
-    cp $GITHUB_WORKSPACE/config/files/config/files/601-tcp_bbr-v3-update-TCP-bbr-congestion-control-module-.patch target/linux/generic/pending-6.18
+    cp $GITHUB_WORKSPACE/config/files/config/files/601-tcp_bbr-v3-update-TCP-bbr-congestion-control-module-.patch openwrt/target/linux/generic/pending-6.18
     echo "✔ [步骤 5] 合入自定义补丁 执行完毕。"
 }
 
